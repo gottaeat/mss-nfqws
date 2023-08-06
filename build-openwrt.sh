2b1b6d229262f66b58dc664e43563657bb9912bc
must_eat <mss@tutanota.de>
2023-08-06 06:05:47 +0300

    upd: (/) build-openwrt.sh; nuke build artifacts.

--- build-openwrt.sh
+++ build-openwrt.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
+_FEED_PATH="${PWD}"/openwrt-feed
+
 mkdir build/
 cd    build/
 
-curl -L -O https://archive.openwrt.org/releases/22.03.5/targets/ramips/mt7621/openwrt-sdk-22.03.5-ramips-mt7621_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+curl -L -O https://archive.openwrt.org/releases/22.03.5/targets/ramips/mt7621\
+/openwrt-sdk-22.03.5-ramips-mt7621_gcc-11.2.0_musl.Linux-x86_64.tar.xz
 
 tar xf openwrt-sdk-22.03.5-ramips-mt7621_gcc-11.2.0_musl.Linux-x86_64.tar.xz
 cd     openwrt-sdk-22.03.5-ramips-mt7621_gcc-11.2.0_musl.Linux-x86_64/
 
-echo "src-link local /mnt/mss/stuff/techy-bits/git/mss-nfqws/openwrt-feed" >> feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
+echo "src-link local ${_FEED_PATH}" >> feeds.conf.default
+./scripts/feeds update packages base local
+./scripts/feeds install mss-nfqws
 
 make defconfig
 make package/mss-nfqws/compile
 
-mv bin/packages/mipsel_24kc/local/mss-nfqws_7299bed8-1_mipsel_24kc.ipk ../../
+mv bin/packages/mipsel_24kc/local/mss-nfqws*.ipk ../../
+cd ../../; rm -rf build/
